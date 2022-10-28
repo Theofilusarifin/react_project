b08f39e203e2fe0ac3d6dafc9882433f719c1c8a
Theofilusarifin <theofilusarifin@gmail.com>
2022-10-28 20:08:58 +0700

    Update Bootstrap

--- learn_react/src/App.jsx
+++ learn_react/src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+export default function app() {
+    return (
+        <div>
+            <button
+                type='button'
+                className='btn btn-info'
+                data-bs-toggle='modal'
+                data-bs-target='#staticBackdrop'
+            >
+                Launch static backdrop modal
+            </button>
+            <div
+                class='modal fade'
+                id='staticBackdrop'
+                data-bs-backdrop='static'
+                data-bs-keyboard='false'
+                tabindex='-1'
+                aria-labelledby='staticBackdropLabel'
+                aria-hidden='true'
+            >
+                <div class='modal-dialog'>
+                    <div class='modal-content'>
+                        <div class='modal-header'>
+                            <h5 class='modal-title' id='staticBackdropLabel'>
+                                Modal title
+                            </h5>
+                            <button
+                                type='button'
+                                class='btn-close'
+                                data-bs-dismiss='modal'
+                                aria-label='Close'
+                            ></button>
+                        </div>
+                        <div class='modal-body'>...</div>
+                        <div class='modal-footer'>
+                            <button
+                                type='button'
+                                class='btn btn-secondary'
+                                data-bs-dismiss='modal'
+                            >
+                                Close
+                            </button>
+                            <button type='button' class='btn btn-primary'>
+                                Understood
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
-
-export default App
--- learn_react/src/main.jsx
+++ learn_react/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+// Import Bootstap from node module
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+// Manggil APP JS
+import App from './App';
+
+// Ambil element ID ROOT
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
